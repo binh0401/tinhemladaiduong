58dc7703d3730d7a3fec32befe662db353ca80a9
Binh Tran <binh412005@gmail.com>
2025-03-05 23:06:38 -0500

    install morgan, helmet, compression

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 import express from 'express'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import compression from 'compression'
 
 export const app = express()
 
-// init middlewares
 
+// init middlewares
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(compression())
 
 //init db
 
 //init router
+
 
 //handling error
 
