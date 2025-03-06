c2c29c894f6466e50e7336971035ff4f7fcb9a33
Binh Tran <binh412005@gmail.com>
2025-03-05 22:41:52 -0500

    init server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+import { app } from "./src/app.js";
+
+const PORT = 3055
+
+const server = app.listen(PORT, () => {
+  console.log(`WS eCommerce start with port ${PORT}`)
+})
+
+process.on('SIGINT', () => {
+  server.close(() => {
+    console.log('Exit Server Express')
+  })
+})
---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+export const app = express()
+
+// init middlewares
+
+
+//init db
+
+//init router
+
+//handling error
+
