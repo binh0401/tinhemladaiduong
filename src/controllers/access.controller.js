6178c85be1637de1b8bcc3f6c55c2acf125d579d
Binh Tran <binh412005@gmail.com>
2025-03-13 22:05:21 -0400

    refactor code

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,20 +1,10 @@
 'use strict'
-
+const AccessService = require('../services/access.service')
 class AccessController{
   signUp = async(req, res, next) => {
     try {
       console.log(`[P]::signUp::`, req.body)
-
-      /*
-        200 OK
-        201 CREATED
-      */
-
-
-      return res.status(201).json({
-        code: '20001',
-        metadata: {userId: 1}
-      })
+      return res.status(201).json(AccessService.signUp(req.body))
     } catch (error) {
       next(error)
     }
--- src/services/access.service.js
+++ src/services/access.service.js
@@ -11,19 +11,19 @@ const shopRoles = {
   ADMIN: 'ADMIN'
 }
 
-class AccessService{
-  static signUp = async({name, email, password}) => {
+class AccessService {
+  static signUp = async ({ name, email, password }) => {
     try {
       //step 1: check email exist ?
       const holderShop = await shopModel.findOne(email).lean() //faster query, return a pure JS object
 
-      if(holderShop){
+      if (holderShop) {
         return {
           code: 'xxx',
           message: 'Shop already registered'
         }
       }
-      
+
       //Save User Data
       const passwordHash = await bcrypt.hash(password, 10)
       const newShop = await shopModel.create({
@@ -35,45 +35,45 @@ class AccessService{
       //#1: Signup, then redirect to login  -> no need for token in the sign up step
       //#2: Signup and give refresh and access token --> this way
 
-      //Generate publickey and private key, store public key into db
-      if(newShop){
-        const {privateKey, publicKey} = crypto.generateKeyPairSync('rsa')
+      //Generate publickey and private key, store public key into db and return publicKey as string
+      if (newShop) {
+        const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa')
 
         const publicKeyString = await KeyTokenService.createKeyToken({
           userId: newShop._id,
           publicKey
         })
 
-        if(!publicKeyString){
+        if (!publicKeyString) {
           return {
             code: 'xxx',
             message: 'publicKeyString error',
           }
         }
-      
 
-      //create a token pair based on publicKey and privateKey. 
-      const tokens = await createTokenPair({
-        userId: newShop._id,
-        email,
-      }, publicKey, privateKey)
 
-      console.log('Create Token Success', tokens)
+        //create a token pair based on publicKey and privateKey. 
+        const tokens = await createTokenPair({
+          userId: newShop._id,
+          email,
+        }, publicKey, privateKey)
+
+        console.log('Create Token Success', tokens)
+
+        return {
+          code: 201,
+          metadata: {
+            shop: newShop,
+            tokens
+          }
+        }
+
+      }
 
       return {
-        code: 201,
-        metadata: {
-          shop: newShop,
-          tokens
-        }
+        code: 200,
+        metadata: null
       }
-    
-    }
-
-    return {
-      code: 200,
-      metadata: null
-    }
     } catch (error) {
       return {
         code: 'xxx',
