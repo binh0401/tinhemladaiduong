5d3be3aa10a1c97df5cfac20268f6e7e0d81674f
Binh Tran <binh412005@gmail.com>
2025-03-13 20:12:05 -0400

    start generate keytoken service

--- src/services/access.service.js
+++ src/services/access.service.js
@@ -1,9 +1,47 @@
 'use strict'
+const shopModel = require('../models/shop.model')
+const bcrypt = require('bcryptjs')
+const crypto = require('crypto')
+const shopRoles = {
+  SHOP: 'SHOP',
+  WRITER: 'WRITER',
+  EDITOR: 'EDITOR',
+  ADMIN: 'ADMIN'
+}
+
 
 class AccessService{
   static signUp = async({name, email, password}) => {
     try {
-      //step 1: 
+      //step 1: check email exist ?
+      const holderShop = await shopModel.findOne(email).lean() //faster query, return a pure JS object
+
+      if(holderShop){
+        return {
+          code: 'xxx',
+          message: 'Shop already registered'
+        }
+      }
+      
+      const passwordHash = await bcrypt.hash(password, 10)
+      const newShop = await shopModel.create({
+        name, email, password: passwordHash, roles: [shopRoles.SHOP]
+      })
+
+
+      //2 ways: 
+      //#1: Signup, then redirect to login  -> no need for token in the sign up step
+      //#2: Signup and give refresh and access token
+
+      if(newShop){
+        const {privateKey, publicKey} = crypto.generateKeyPairSync('rsa')
+
+        
+
+      }
+
+
+
     } catch (error) {
       return {
         code: 'xxx',
---
+++ src/services/keyToken.service.js
@@ -0,0 +1,15 @@
+'use strict'
+
+class KeyTokenService{
+
+    static createKeyToken = async({user, publicKey}){
+      try {
+        
+      } catch (error) {
+        
+      }
+    }
+}   
+
+
+module.exports = KeyTokenService
