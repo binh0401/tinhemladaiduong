73ee8d069d278098f523b426d3eb4a82b526ee4a
Binh Tran <binh412005@gmail.com>
2025-03-16 14:29:56 -0400

    start apply new pattern: factory x strategy

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,28 +1,28 @@
 'use strict'
 
-const {product, clothing, electronic} = require('../models/product.model')
-const {BadRequestError} = require('../core/error.response')
+const { product, clothing, electronic } = require('../models/product.model')
+const { BadRequestError } = require('../core/error.response')
 
 //Apply Factory Pattern
 class ProductFactory {
-  static async createProduct(type, payload){   //payload: {shop's payload}
-      switch (type){
-        case 'Electronic':
-          return new Electronic(payload).createProduct()
-        case 'Clothing':
-          return new Clothing(payload).createProduct()
-        default: 
-          throw new BadRequestError(`Invalid Product Category: ${type}`)
-      }
+  static async createProduct(type, payload) {   //payload: {shop's payload}
+    switch (type) {
+      case 'Electronic':
+        return new Electronic(payload).createProduct()
+      case 'Clothing':
+        return new Clothing(payload).createProduct()
+      default:
+        throw new BadRequestError(`Invalid Product Category: ${type}`)
+    }
   }
 }
 
-class Product{
+class Product {
   constructor({
     product_name, product_thumb, product_description,
     product_price, product_quantity, product_type, product_shop,
     product_attributes
-  }){
+  }) {
     this.product_name = product_name
     this.product_thumb = product_thumb
     this.product_description = product_description
@@ -34,47 +34,47 @@ class Product{
   }
 
   //create new product
-  async createProduct(_id){
-    return await product.create({...this, _id})
+  async createProduct(_id) {
+    return await product.create({ ...this, _id })
   }
 
 }
 
 //Sub-class for clothing
-class Clothing extends Product{
+class Clothing extends Product {
 
   //automatically call constructor of Products
   //Or we can explicitly define a constructor
   // constructor(payload){
   //   super(payload)
   // }
-  
-  async createProduct(){
+
+  async createProduct() {
     const newClothing = await clothing.create({
       ...this.product_attributes,
       product_shop: this.product_shop
     })
-    if(!newClothing) throw new BadRequestError('Create new Clothing Error')
-    
+    if (!newClothing) throw new BadRequestError('Create new Clothing Error')
+
     const newProduct = await super.createProduct()
-    if(!newProduct) throw new BadRequestError('Create new Product Error')
+    if (!newProduct) throw new BadRequestError('Create new Product Error')
 
     return newProduct
   }
 }
 
 //Sub-class for electronic
-class Electronic extends Product{
+class Electronic extends Product {
 
-  async createProduct(){
+  async createProduct() {
     const newElectronic = await electronic.create({
       ...this.product_attributes,
       product_shop: this.product_shop
     })
-    if(!newElectronic) throw new BadRequestError('Create new Electronic Error')
-    
+    if (!newElectronic) throw new BadRequestError('Create new Electronic Error')
+
     const newProduct = await super.createProduct(newElectronic._id)
-    if(!newProduct) throw new BadRequestError('Create new Product Error')
+    if (!newProduct) throw new BadRequestError('Create new Product Error')
 
     return newProduct
   }
