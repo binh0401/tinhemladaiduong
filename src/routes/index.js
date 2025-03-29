60995c2dc777d8b38669f55d2b92b1dff1c686d1
Binh Tran <binh412005@gmail.com>
2025-03-29 14:29:24 -0400

    cart model

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ const router = express.Router()
 const accessRoutes = require('./access/index.js')
 const productRoutes = require('./product/index.js')
 const discountRoutes = require('./discount/index.js')
+const cartRoutes = require('./cart/index.js')
 const { apiKey, permission } = require('../auth/checkAuth.js')
 
 // check apiKey
@@ -18,5 +19,5 @@ router.use(permission('0000'))
 router.use('/v1/api/product', productRoutes)
 router.use('/v1/api/shop', accessRoutes)
 router.use('/v1/api/discount', discountRoutes)
-
+router.use('/v1/api/cart', cartRoutes)
 module.exports = router
--- src/services/cart.service.js
+++ src/services/cart.service.js
@@ -1,1 +1,22 @@
 'use strict'
+
+/*
+  1, Add product to cart : User
+  2, Reduce product quantity: User
+  3, Increase product quantity: User
+  4, Get products of cart: User
+  5, Delete cart: User
+  6, Delete cart item: User
+*/
+
+class CartService{
+
+  //1
+  static async addToCart({user_id, products = {}}){
+      
+  }
+
+
+}
+
+module.exports = CartService
