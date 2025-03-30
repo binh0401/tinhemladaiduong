8f6b10b9109a9a3e9acaba488830c0c6d487c471
Binh Tran <binh412005@gmail.com>
2025-03-30 14:15:55 -0400

    finish cart routes

---
+++ src/controllers/cart.controller.js
@@ -0,0 +1,55 @@
+'use strict'
+const {OK, CREATED} = require('../core/success.response')
+const CartService = require('../services/cart.service')
+
+class CartController {
+    //1.
+    addToCart = async (req,res,next) => {
+      const user_id = req.params.userId
+      const {product} = req.body
+      new CREATED({
+        message: 'Add product to cart success',
+        metadata: await CartService.addToCart({user_id, product})
+      }).send(res)
+    }
+
+    //2
+    updateCart = async(req,res,next) => {
+      const user_id = req.params.userId
+      const {shop_order_ids} = req.body
+      new OK({
+        message: 'Update cart success',
+        metadata: await CartService.updateCart({user_id, shop_order_ids})
+      }).send(res)
+    }
+
+    //3
+    deleteProductOfCart = async(req,res,next) => {
+      const user_id = req.params.userId
+      const {product_id} = req.body
+      new OK({
+        message: 'Delete product in cart success',
+        metadata: await CartService.deleteProductOfCart({user_id, product_id})
+      }).send(res)
+    }
+
+    //4
+    getProductsInCart = async(req,res,next) => {
+      const user_id = req.params.userId 
+      new OK({
+        message: 'Get products in cart success',
+        metadata: await CartService.getProductsInCart({user_id})
+      }).send(res)
+    }
+
+    //5
+    deleteAllCart = async(req,res,next) => {
+      const user_id = req.params.userId 
+      new OK({
+        message: 'Get products in cart success',
+        metadata: await CartService.deleteAllCart({user_id})
+      }).send(res)
+    }
+}
+
+module.exports = new CartController()
--- src/routes/cart/index.js
+++ src/routes/cart/index.js
@@ -1,8 +1,22 @@
 'use strict'
-
+const asyncHandler = require('../../helpers/asyncHandler')
 const express = require('express')
-
+const cartController = require('../../controllers/cart.controller')
 const router = express.Router()
 
+//1
+router.post('/add/:userId', asyncHandler(cartController.addToCart))
+
+//2
+router.post('/update/:userId', asyncHandler(cartController.updateCart))
+
+//3
+router.post('/delOne/:userId', asyncHandler(cartController.deleteProductOfCart))
+
+//4
+router.get('/all/:userId', asyncHandler(cartController.getProductsInCart))
+
+//5
+router.delete('/delAll/:userId', asyncHandler(cartController.deleteAllCart))
 
 module.exports = router
