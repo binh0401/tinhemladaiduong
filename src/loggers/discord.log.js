976a28784664dca4c4a4606dec65385c5ab7efa5
Binh Tran <binh412005@gmail.com>
2025-04-12 19:36:49 -0400

    adding discord logger bot

--- src/loggers/discord.log.js
+++ src/loggers/discord.log.js
@@ -1,23 +1,10 @@
 'use strict'
 import {Client, GatewayIntentBits} from 'discord.js'
 
-
-// client.on('ready', () => {
-//   console.log(`Logged in as ${client.user.tag}`)
-// })
-
-// 
-
-// client.on('messageCreate', message => {
-//   if(message.author.bot){
-//     return
-//   }
-//   if(message.content === 'a thèm bú lồn quá'){
-//     message.reply('Oke đi muốn bú lồn em nào')
-//   }else{
-//     message.reply("im con cụ mày mồm vào")
-//   }
-// })
+const DISCORD_TOKEN = "MTM2MDcwMDk5MDg5Mzk4MTcxOA.Gw_JwY.ZVCXlqXlqgxWEbDvgpFL86xgUoSiKuxgdUrVYc"
+const DISCORD_CHANNEL_ID = "839874422923460609"
+ 
+const msg_reply = ['mày sủa vừa vừa thôi', 'anh là máy chém bách khoa', 'địt cụ mày thằng hà nội 2', 'thằng khải', 'vãi lồn luôn', 'sv đàm', 'thuần bợ']
 
 class LoggerService{
   constructor(){
@@ -29,11 +16,40 @@ class LoggerService{
         GatewayIntentBits.MessageContent
       ]
     })
+    
+    //channelId
+    this.channel_id = DISCORD_CHANNEL_ID
 
-    client.login("MTM2MDcwMDk5MDg5Mzk4MTcxOA.Gw_JwY.ZVCXlqXlqgxWEbDvgpFL86xgUoSiKuxgdUrVYc")
+    this.client.on('ready', () => {
+      console.log(`Logged as ${this.client.user.tag}`)
+    })
+
+    this.client.login(DISCORD_TOKEN)
+
+    this.client.on('messageCreate', (msg) => {
+      if(msg.author.bot){
+        return
+      }
+      if(msg.content.includes('hát')){
+        msg.reply(`nần ná na na anh ${msg.author.username} \n https://www.youtube.com/watch?v=iGxeKsT1rN0`)
+      }else if(msg.content.includes('sex')){
+        msg.reply(`ok đi thèm bú lồn em nào, lên luôn này con vợ ${msg.author.username} \n https://www.pornhub.com/`)
+      }else{
+        let index = Math.floor(Math.random() * msg_reply.length)
+        msg.reply(` ${msg_reply[index]}.Im con cụ mày mồm vào thằng ${msg.author.username}.`)
+      }
+    })
   }
 
+  sendToDiscord(message){
+    const channel = this.client.channels.cache.get(this.channel_id)
+    if(!channel){
+      console.log('Cant find channel')
+      return
+    }
 
+    channel.send(message).cache(err => console.log(err))
+  }
 
 }
 
