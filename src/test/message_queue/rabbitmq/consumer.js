55a447ac0132d83a7d8e7a5a4579e0f06d869217
Binh Tran <binh412005@gmail.com>
2025-04-21 06:07:50 -0400

    rabbitmq test

---
+++ src/test/message_queue/rabbitmq/consumer.js
@@ -0,0 +1,22 @@
+import amqplib from 'amqplib'
+
+const runConsumer = async() => {
+  try {
+    const connect = await amqplib.connect('amqp://localhost')
+    const channel = await connect.createChannel()
+
+    const queue_name = 'q1'
+    await channel.assertQueue(queue_name, {
+      durable: true
+    })
+
+    channel.consume(queue_name, (msg) => {
+      console.log(msg)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+runConsumer()
+
---
+++ src/test/message_queue/rabbitmq/producer.js
@@ -0,0 +1,26 @@
+import amqplib from 'amqplib'
+
+const message = 'hello'
+
+const runProducer = async (msg) => {
+  try {
+    const connect = await amqplib.connect('amqp://localhost')
+    
+    const channel = await connect.createChannel()
+
+    const queue_name = 'q1'
+    
+    await channel.assertQueue(queue_name, {
+      durable: true
+    })
+
+    //publish message
+    await channel.sendToQueue(queue_name, Buffer.from(msg))
+    
+
+  } catch (error) { 
+    console.log(error)
+  }
+}
+
+runProducer(message)
